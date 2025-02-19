609df56d8426186faf913092dc73ea2f3007e733
notigayatri <gayatrin005@gmail.com>
2025-02-19 15:56:16 +0530

    database connection

--- day3/db1.py
+++ day3/db1.py
@@ -1,12 +1,19 @@
 import pymysql
 def connect_db():
-    connection=pymysql.Connect(host='localhost',port=3306,user='root',password='root',database='gayatri_db',charset='utf8')
-    print("database connected")
-    return connection
-
+    try:
+        connection=pymysql.Connect(host='localhost',port=3306,user='root',password='root',database='gayatri_db',charset='utf8')
+        print("database connected")
+        return connection
+    except:
+        print('database connection failed')
+    
 def disconnect_db(connection):
-    connection.close()
-    print("database disconnected")
+    try:
+        connection.close()
+        print("database disconnected")
+    except:
+        print('database diconnection failed')
 
 connection=connect_db()
-disconnect_db(connection)
+if connection:
+    disconnect_db(connection)
