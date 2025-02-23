67ae6cc60e1b354c9e57bab45f535657a13626f1
notigayatri <gayatrin005@gmail.com>
2025-02-24 00:37:57 +0530

    modified person flask app

--- day6/person_dao.py
+++ day6/person_dao.py
@@ -1,14 +1,14 @@
 import pymysql
 
 class Person:
-    def __init__(self):
-        self.name =""
-        self.gender=""
-        self.dob=""
-        self.location=""
+    def __init__(self, name="", gender="", dob="", location=""):
+        self.name       = name
+        self.gender     = gender
+        self.dob        = dob
+        self.location   = location
 
     def __str__(self):
-        print(f"Name:{self.name}, Location:{self.location}")
+        print(f'Name:{self.name}, Location:{self.location}')
 
 class Db_operations:
     def __init__(self):
@@ -16,18 +16,18 @@ class Db_operations:
 
     def connect_db(self):
         try:
-            connection=pymysql.Connect(host='localhost',port=3306,user='root',password='root',database='gayatri_db',charset='utf8')
-            print("database connected")
+            connection = pymysql.Connect(host='localhost', port=3306, user='root', password='root', database='gayatri_db', charset='utf8')
+            print('DB connected')
             return connection
         except:
-            print('database connection failed')
-        
-    def disconnect_db(self,connection):
+            print('DB connection failed')
+
+    def disconnect_db(self, connection):
         try:
             connection.close()
-            print("database disconnected")
+            print('DB dis-connected')
         except:
-            print('database diconnection failed')
+            print('Error while disconnecting DB')
 
     def create_db(self):
         connection = self.connect_db()
@@ -36,27 +36,26 @@ class Db_operations:
         cursor.execute(query)
         cursor.close()
         print('DB created')
+        self.disconnect_db(connection)
 
     def create_table(self):
         connection = self.connect_db()
-        query = "create table IF NOT EXISTS person(id int primary key auto_increment, name varchar(32) not null, gender char check(gender in('m','M', 'f','F')), location varchar(32), dob date);"
+        query = "create table IF NOT EXISTS persons(id int primary key auto_increment, name varchar(32) not null, gender char check(gender in('m','M', 'f','F')), location varchar(32), dob date);"
         cursor = connection.cursor()
         cursor.execute(query)
         cursor.close()
         print('Table created')
         self.disconnect_db(connection)
 
-
     def read_person_details(self):
         name = input('Enter person name: ')
         gender = input('Enter person gender: ')[0]
         location = input('Enter person location: ')
-        dob = input('Enter person date of birth(yyyy-mm-dd): ')
+        dob = input('Enter person date of borth(yyyy-mm-dd): ')
         return (name, gender, location, dob)
 
     def insert_row(self, person):
-        #person = self.read_person_details()
-        query = 'insert into person(name, gender, location, dob) values(%s, %s, %s, %s);'
+        query = 'insert into persons(name, gender, location, dob) values(%s, %s, %s, %s);'
         person_tuple = (person.name, person.gender, person.location, person.dob)
         connection = self.connect_db()
         cursor = connection.cursor()
@@ -67,11 +66,8 @@ class Db_operations:
         id = self.get_latest_row_id()
         return id
 
-    def update_row(self):
-        new_location = input('Enter new location of the person: ')
-        id = int(input('Enter Id of the person to update the location: '))
-        data = (new_location, id)
-        query = f'update person set location = %s where id = %s'
+    def update_row(self, data):
+        query = f'update persons set name = %s, gender = %s, location = %s, dob = %s where id = %s'
         connection = self.connect_db()
         cursor = connection.cursor()
         cursor.execute(query, data)
@@ -79,9 +75,9 @@ class Db_operations:
         cursor.close()
         self.disconnect_db(connection)
 
-    def delete_row(self):
-        id = int(input('Enter Id of the person to delete: '))
-        query = f'delete from person where id = {id}'
+    def delete_row(self, id):
+        #id = int(input('Enter Id of the person to delete: '))
+        query = f'delete from persons where id = {id}'
         connection = self.connect_db()
         cursor = connection.cursor()
         count = cursor.execute(query)
@@ -96,7 +92,7 @@ class Db_operations:
     def search_row(self, id):
         row = None
         #id = int(input('Enter Id of the person to search: '))
-        query = f'select * from person where id = {id}'
+        query = f'select * from persons where id = {id}'
         connection = self.connect_db()
         cursor = connection.cursor()
         count = cursor.execute(query)
@@ -111,22 +107,25 @@ class Db_operations:
         return row
 
     def list_all_rows(self):
-        query = f'select * from person;'
-        connection = self.connect_db()
-        cursor = connection.cursor()
-        count = cursor.execute(query)
-        if count == 0:
-            print(f'No rows found in the table')
-        else:
-            rows = cursor.fetchall()
-            for row in rows:
-                print(str(row))
-        connection.commit()
-        cursor.close()
-        self.disconnect_db(connection)
+        query = 'select * from persons;'
+        try:
+            connection = self.connect_db()
+            cursor = connection.cursor()
+            count = cursor.execute(query)
+            if count == 0:
+                print(f'No rows found in the table')
+            else:
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(str(row))
+            cursor.close()
+            self.disconnect_db(connection)
+            return rows
+        except:
+            print('Error in reading rows')
 
     def get_latest_row_id(self):
-        query = 'select max(id) from person;'
+        query = 'select max(id) from persons;'
         connection = self.connect_db()
         cursor = connection.cursor()
         cursor.execute(query)
